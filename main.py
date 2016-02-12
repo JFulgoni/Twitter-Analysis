fff7a3e9293aa7f17a1d7f760e80a05e8a7d3169
Jetoff <johnflgn@gmail.com>
2016-02-12 11:56:20 -0500

    Added main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from __future__ import division
+import sys
+import time
+import nltk
+import string
+
+__author__ = 'johnfulgoni'
+
+if __name__ == '__main__':
+    print "Hello World"
+    print 2 / 3
+    print time.time()
